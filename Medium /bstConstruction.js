bcdb600ee38f278142bcc7b75572d732b011d714
Kai <kaiyip.ho23@gmail.com>
2021-03-11 20:54:50 -0500

    bst done

--- Medium /bstConstruction.js
+++ Medium /bstConstruction.js
@@ -7,41 +7,124 @@ class BST {
 
   insert(value) {
     
-    if (this.value > value){
-      if (this.right === null){
-        this.right = new BST(value);
+		if (value < this.value){
+			if (this.left === null){
+				this.left = new BST(value)
+			} else {
+				this.left.insert(value);
+			}
+		} else {
+			if (this.right === null){
+				this.right = new BST(value);
+			} else {
+				this.right.insert(value);
+			}
+		}
+    return this;
+  }
+
+contains(value) {
+    let currentValue = this.value;
+
+    if (currentValue > value){
+      if (this.left === null){
+        return false;
       } else {
-        this.right.insert(value);
+        return this.left.contains(value);
+      }
+    } else if (value > currentValue){
+      if (this.right === null){
+        return false;
+      } else {
+        return this.right.contains(value);
       }
     } else {
-      if (this.left === null){
-        this.left = new BST(value);
-      } else {
-        this.left.insert(value);
+      return true;
+    }
+  }
+
+  remove(value, parent = null) {
+    if (this.value > value){
+      if (this.left !== null){
+        this.left.remove(value, this)
+      }
+    } else if (value > this.value){
+      if (this.right !== null){
+        this.right.remove(value, this);
+      }
+    } else {
+      if (this.left !== null && this.right !== null){ //if has two child, always take min from right as replacement so all left will be less
+        this.value = this.right.returnMin();
+        this.right.remove(this.value, this);
+      } else if (parent === null){ //if we removing root node, and the root only has either left or right we reassign
+        if (this.left !== null){
+          this.value = this.left.value;
+          this.right = this.left.right;
+          this.left = this.left.left;
+        } else if (this.right !== null){
+          this.value = this.right.value;
+          this.right = this.right.right;
+          this.left = this.right.left;
+        } else { // if it only has itself and no child nodes ask interviewer 
+
+        }
+      } else if (parent.left === this){ // we have found value and it only has one child node, need to reassign 
+        parent.left = this.left !== null ? this.left : this.right;
+      } else if (parent.right === this){
+        parent.right = this.left !== null ? this.left : this.right;
       }
     }
     return this;
   }
 
-  contains(value) {
-    let currentValue = this.value;
 
-    if (currentValue > value && this.left !== null){
-      this.left.contains(value);
-    } else if (currentValue < value && this.right !== null){
-      this.right.contains(value);
-    } else if (currentValue === value){
-      return true;
-    } else if (this.value === null){
-      return false;
-    } 
-
-    // Write your code here.
-  }
-
-  remove(value) {
-    // Write your code here.
-    // Do not edit the return statement of this method.
-    return this;
+  returnMin(){
+    if (this.left === null){
+      return this.value;
+    } else {
+      return this.left.returnMin();
+    }
   }
 }
+
+[
+  {
+    "arguments": [5],
+    "method": "insert",
+    "output": null,
+    "tree": {
+      "nodes": [
+        {"id": "10", "left": "5", "right": null, "value": 10},
+        {"id": "5", "left": null, "right": null, "value": 5}
+      ],
+      "root": "10"
+    }
+  },
+  {
+    "arguments": [15],
+    "method": "insert",
+    "output": null,
+    "tree": {
+      "nodes": [
+        {"id": "10", "left": "5", "right": "15", "value": 10},
+        {"id": "15", "left": null, "right": null, "value": 15},
+        {"id": "5", "left": null, "right": null, "value": 5}
+      ],
+      "root": "10"
+    }
+  },
+  {
+    "arguments": [2],
+    "method": "insert",
+    "output": null,
+    "tree": {
+      "nodes": [
+        {"id": "10", "left": "5", "right": "15", "value": 10},
+        {"id": "15", "left": null, "right": null, "value": 15},
+        {"id": "5", "left": "2", "right": null, "value": 5},
+        {"id": "2", "left": null, "right": null, "value": 2}
+      ],
+      "root": "10"
+    }
+  },
+  {
