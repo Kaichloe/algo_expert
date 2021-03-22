75b2cd4330356e822134489198e017f9c7cb7c97
Kai <kaiyip.ho23@gmail.com>
2021-03-21 23:32:17 -0400

    ways

---
+++ Medium /numOfwayToMakeChange.js
@@ -0,0 +1,15 @@
+// denoms = num of different coins you can use, n = the target amount to make
+// 
+function numberOfWaysToMakeChange(n, denoms) {
+  const ways = new Array(n + 1).fill(0);
+  ways[0] = 1;
+
+  for (const denom of denoms){
+    for (let amount = 1; amount < n + 1; amount++) {
+      if (denom <= amount){
+        ways[amount] += ways[amount - denom];
+      }
+    }
+  }
+  return ways[n];
+}
