0f4632e3d0eee2107d63b9536fdfbb049a8597e0
Kai <kaiyip.ho23@gmail.com>
2021-04-01 22:48:10 -0400

    coins

--- Medium /findSuccessor.js
+++ Medium /findSuccessor.js
@@ -28,3 +28,27 @@ function inOrderTraverse(node, order = []){
   return order;
 }
 
+// ON and ON where N is # of node
+
+function findSuccessor(tree, node){
+  if (node.right !== null) return getLeftmostChild(node.right);
+  return getRightmostParent(node);
+}
+
+function getLeftmostChild(node){
+  let currentNode = node;
+  while (currentNode.left !== null){
+    currentNode = currentNode.left;
+  }
+  return currentNode;
+}
+
+function getRightmostParent(node){
+  let currentNode = node;
+  while (currentNode.parent !== null && currentNode.parent.right === currentNode){
+    currentNode = currentNode.parent;
+  }
+  return currentNode.parent;
+}
+
+// OH and O1 where H is the height of the tree
