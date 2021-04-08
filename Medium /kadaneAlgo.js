d00770df94e44894aebb9d4abb27c64c1bf00ab9
Kai <kaiyip.ho23@gmail.com>
2021-04-07 22:43:41 -0400

    levenshtein done

--- Medium /kadaneAlgo.js
+++ Medium /kadaneAlgo.js
@@ -1,3 +1,12 @@
+// take in non empty arr of inters returns max sum of subarray (inters must be adjacent)
 function kadanesAlgorithm(array) {
-  // Write your code here.
+  let maxEnd = array[0];
+  let maxSoFar = array[0];
+
+  for (let i = 1; i < array.length; i++) {
+    const num = array[i];
+    maxEnd = Math.max(num, maxEnd + num);
+    maxSoFar = Math.max(maxSoFar, maxEnd)
+  }
+  return maxSoFar;
 }
