5255f9bb2bf87b60c1513d06b5a85c0a9536d2cb
Kai <kaiyip.ho23@gmail.com>
2021-04-27 19:32:19 -0400

    push

--- Medium /riverSizes.js
+++ Medium /riverSizes.js
@@ -28,3 +28,47 @@ function riverChecker(row,column, matrix){
   riverChecker(row, column+1, matrix)
   riverChecker(row, column-1, matrix)
 }
+
+
+// 
+function riverSizes(matrix){
+  const sizes = [];
+  const visted =matrix.map(row => row.map(value => false)); //make a dup matrix where all values are false
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (visted[i][j]) continue; //if it has been visted, if true then go to next one
+      traverseNode(i, j, matrix, visted, sizes);// this hits if visted is false
+    }
+  }
+  return sizes;
+}
+
+function traverseNode(i, j, matrix, visted, sizes){
+  let currentRiverSize = 0;
+  const nodesToExplore = [[i,j]];
+  while (nodesToExplore.length){
+    const currentNode = nodesToExplore.pop();
+    i = currentNode[0];
+    j = currentNode[1];
+    if (visted[i][j]) continue;
+    visted[i][j] = true;
+    if (matrix[i][j] === 0) continue;
+    currentRiverSize++;
+    const unvistedNeighors = getUnvistedNeighbors(i, j, matrix, visted);
+    for (const neighbor of unvistedNeighors){
+      nodesToExplore.push(neighbor);  
+    }
+  }
+  if (currentRiverSize > 0) sizes.push(currentRiverSize);
+}
+
+function getUnvistedNeighbors(i, j, matrix, visted){
+  const unvistedNeighors = [];
+  if (i > 0 && !visted[i - 1][j]) unvistedNeighors.push([i - 1, j]);
+  if (i < matrix.length - 1 && !visted[i + 1][j]) unvistedNeighors.push([i + 1, j]);
+  if (j > 0 && !visted[i][j - 1]) unvistedNeighors.push([i, j -1]);
+  if (j < matrix[0].length - 1 && !visted[i][j + 1]) unvistedNeighors.push([i, j + 1]);
+  return unvistedNeighors;
+}
+
+//O(wh) and O(wh) where w and h are the width and height of the input matrix
